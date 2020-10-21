72d54a36eb06456663270e8dcf0b9a8b870a4bd7
alireza-shirmohammadi <alireza.sh076@gmail.com>
2020-10-21 15:30:49 -0700

    12

--- manager/urls.py
+++ manager/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     url(r'^panel/manager/list/$', views.manager_list, name='manager_list'),
     url(r'^panel/manager/del/(?P<pk>\d+)/$', views.manager_del, name='manager_del'),
+    url(r'^panel/manager/group/$', views.manager_group, name='manager_group'),
+    url(r'^panel/manager/group/add/$', views.manager_group_add, name='manager_group_add'),
+    url(r'^panel/manager/group/del/(?P<name>.*)/$', views.manager_group_del, name='manager_group_del'),
+    url(r'^panel/manager/group/show/(?P<pk>\d+)/$', views.users_groups, name='users_groups'),
+    url(r'^panel/manager/addtogroup/(?P<pk>\d+)/$', views.add_users_to_groups, name='add_users_to_groups'),
+     url(r'^panel/manager/delgroup/(?P<pk>\d+)/(?P<name>.*)/$', views.del_users_to_groups, name='del_users_to_groups'),
 ]
--- manager/views.py
+++ manager/views.py
@@ -7,7 +7,7 @@ from cat.models import Cat
 from manager.models import Manager
 from django.contrib.auth import authenticate, login, logout
 from django.core.files.storage import FileSystemStorage
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 def manager_list (request):
      # login check start
@@ -31,3 +31,138 @@ def manager_del (request,pk):
         manager.delete()
 
         return redirect('manager_list')
+
+def manager_group (request):
+     # login check start
+    if not request.user.is_authenticated :
+        return redirect('mylogin')
+    # login check end
+
+
+    perm = 0
+    for i in request.user.groups.all():
+        if i.name == "masteruser" : perm = 1
+
+    if perm == 0 :
+        error = "Access Denied"
+        return render(request, 'back/error.html' , {'error':error})
+
+
+    group=Group.objects.all()
+
+    return render (request,'back/manager_group.html',{'group':group})
+
+def manager_group_add (request):
+     # login check start
+    if not request.user.is_authenticated :
+        return redirect('mylogin')
+    # login check end
+
+
+    perm = 0
+    for i in request.user.groups.all():
+        if i.name == "masteruser" : perm = 1
+
+    if perm == 0 :
+        error = "Access Denied"
+        return render(request, 'back/error.html' , {'error':error})
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        if not name == "":
+            if len (Group.objects.filter(name=name))==0 :
+                group=Group(name=name)
+                group.save()
+            else:
+                error = "This Group Name Is Already Exist."
+                return render(request, 'back/error.html' , {'error':error})
+    return redirect('manager_group')
+
+def manager_group_del (request,name):
+     # login check start
+    if not request.user.is_authenticated :
+        return redirect('mylogin')
+    # login check end
+
+
+    perm = 0
+    for i in request.user.groups.all():
+        if i.name == "masteruser" : perm = 1
+
+    if perm == 0 :
+        error = "Access Denied"
+        return render(request, 'back/error.html' , {'error':error})
+
+    group = Group.objects.filter(name=name)
+    group.delete()
+    return redirect('manager_group')
+
+def users_groups(request,pk):
+     # login check start
+    if not request.user.is_authenticated :
+        return redirect('mylogin')
+    # login check end
+
+
+    perm = 0
+    for i in request.user.groups.all():
+        if i.name == "masteruser" : perm = 1
+
+    if perm == 0 :
+        error = "Access Denied"
+        return render(request, 'back/error.html' , {'error':error})
+
+    manager=Manager.objects.get(pk=pk)
+    user=User.objects.get(username=manager.utext)
+    ugroup=[]
+    for i in user.groups.all():
+        ugroup.append(i.name)
+
+    group=Group.objects.all()
+
+    return render(request,'back/users_groups.html',{'group':group,'ugroup':ugroup,'pk':pk})
+
+def add_users_to_groups(request,pk):
+     # login check start
+    if not request.user.is_authenticated :
+        return redirect('mylogin')
+    # login check end
+
+
+    perm = 0
+    for i in request.user.groups.all():
+        if i.name == "masteruser" : perm = 1
+
+    if perm == 0 :
+        error = "Access Denied"
+        return render(request, 'back/error.html' , {'error':error})
+
+    if request.method == 'POST':
+        gname=request.POST.get('gname')
+        group=Group.objects.get(name=gname)
+        manager=Manager.objects.get(pk=pk)
+        user=User.objects.get(username=manager.utext)
+        user.groups.add(group)
+
+    return redirect('users_groups' , pk=pk)
+
+def del_users_to_groups(request,pk,name):
+     # login check start
+    if not request.user.is_authenticated :
+        return redirect('mylogin')
+    # login check end
+
+
+    perm = 0
+    for i in request.user.groups.all():
+        if i.name == "masteruser" : perm = 1
+
+    if perm == 0 :
+        error = "Access Denied"
+        return render(request, 'back/error.html' , {'error':error})
+
+    manager=Manager.objects.get(pk=pk)
+    group=Group.objects.get(name=name)
+    user=User.objects.get(username=manager.utext)
+    user.groups.remove(group)
+    return redirect('users_groups', pk=pk)
